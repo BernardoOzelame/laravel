96d68284e546d54fcfea2dab7316b8c83f094fd9
Bernardo Ozelame Spoltti <ozelamebernardo@gmail.com>
2024-07-18 11:19:25 -0300

    Aula 18/07/2024 pt.2

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,17 +5,22 @@
     <body>
         <h1>@yield('titulo')</h1>
         <hr>
+
         <a href="{{ route('index') }}">Inicial</a>
         |
         <a href="{{ route('funcionarios') }}">Funcionários</a>
-        |
-        <a href="{{ route('usuarios') }}">Usuários</a>
+
+        @if(Auth::user() && Auth::user()['admin'] === 1)
+            |
+            <a href="{{ route('usuarios') }}">Usuários</a>
+        @endif
+
         |
         @if (Auth::user())
-        Olá, <strong>{{ Auth::user()['name'] }}</strong>.
-        <a href="{{route('logout')}}">Logout</a>
+            Olá, <strong>{{ Auth::user()['name'] }}</strong>!
+            <a href="{{route('logout')}}">Logout</a>
         @else
-        <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('login') }}">Login</a>
         @endif
         <hr>
         @yield('conteudo')
