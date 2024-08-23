5540def5fe79596a906d25d373f58fd990b00265
Bernardo Ozelame Spoltti <104913275+BernardoOzelame@users.noreply.github.com>
2024-08-23 11:34:54 -0300

    Aula 23/08/2024

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
     public function run(): void {
-        $this->call(
-            FuncionariosSeeder::class
-        );
+        $this->call([
+            FuncionariosSeeder::class,
+            UsuariosSeeder::class
+        ]);
     }
 }
